21de7515e553b1f12e45226489e78b5d7c44f5b4
Cosmin Albulescu <cosmin.albulescu@digital-education.com>
2014-11-27 16:52:19 +0200

    Fix commits file

--- filters/commits.sh
+++ filters/commits.sh
@@ -3,11 +3,10 @@
 if [ $HOOK != "commit-msg" ]; then exit 0; fi
 
 REG=${INI__commits__regexp/\{\{BRANCH\}\}/$BRANCH}
+COMMIT=$(cat $1)
 
-if [ -n "${INI__commits__regexp}" ]; then
-    if ! [[ $1 =~  $REG ]]; then
+if [ -n "${REG}" ]; then
+    if ! [[ $COMMIT =~  $REG ]]; then
         deny "COMMITS" "Invalid commit message! Should match $REG"
     fi
 fi
-
-exit 1
