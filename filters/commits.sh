19bbfd12c9353ac9d814d73db55ec251a70f9fea
Albulescu Cosmin <cosmin@albulescu.ro>
2014-11-27 21:42:23 +0200

    Check if commit is made on the right branch

--- filters/commits.sh
+++ filters/commits.sh
@@ -10,3 +10,14 @@ if [ -n "${REG}" ]; then
         deny "COMMITS" "Invalid commit message! Should match $REG"
     fi
 fi
+
+if [ -n "${INI__commits__branch}" ] && [ ${#BASH_REMATCH[@]} -eq "1" ]; then
+	notify-send -u critical -t 7000 "Git Hooks" "Option to match branch used but no match from commit regexp"
+	exit 0
+fi
+
+BRANCH_MATCH=${INI__commits__branch/\{\{MATCH\}\}/${BASH_REMATCH[1]}} 
+
+if ! [[ $BRANCH =~  $BRANCH_MATCH ]]; then
+    deny "COMMITS" "Invalid commit message! Not on the right branch '$BRANCH'"
+fi
