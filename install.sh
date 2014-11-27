4956cc57e3624f53fe84de4979f3d08d495cf491
Cosmin Albulescu <cosmin.albulescu@digital-education.com>
2014-11-27 12:48:53 +0200

    Install script

--- install.sh
+++ install.sh
@@ -1,33 +1,107 @@
 #!/usr/bin/env bash
 
-shopt -s extglob
+set -e
 set -o errtrace
 set -o errexit
 
 GITHOOKS_PATH="$HOME/githooks"
+GITHOOKS_HOOK="$GITHOOKS_PATH/hook.sh"
 REPO="git@github.com:albulescu/githooks.git"
-
+CURRENT_PATH=`pwd`
 
 log()  { printf "%b\n" "$*"; }
-debug(){ [[ ${rvm_debug_flag:-0} -eq 0 ]] || printf "%b\n" "Running($#): $*"; }
-fail() { log "\nERROR: $*\n" ; exit 1 ; }
+
+fail() { log "\e[91mERROR: $*\n" ; exit 1 ; }
+
+color_light_yellow() { echo -e "\e[92m$1\e[0m"; }
+color_light_red() { echo -e "\e[31m$1\e[0m"; }
+color_green() { echo -e "\e[32m$1\e[0m"; }
+color_cyan() { echo -e "\e[36m$1\e[0m"; }
+color_dark_gray() { echo -e "\e[90m$1\e[0m"; }
+color_magenta() { echo -e "\e[95m$1\e[0m"; }
+
+link_hook() {
+
+    echo -n "Linking $1..."
+    
+    if [ -L "$1" ] && [ $(readlink -f $1) == "$GITHOOKS_HOOK" ]; then 
+        echo -e $(color_dark_gray "[already linked]")
+        return 0
+    fi
+
+    if [ -f "$1" ];
+    then
+        cp "$1" "$1.backup"
+        
+        rm -rf $1
+        
+        ln -s "$GITHOOKS_HOOK" $1
+
+        echo -n $(color_green "[linked]")
+
+        echo $(color_magenta "[backup]")
+    else
+
+        ln -s "$GITHOOKS_HOOK" $1
+        echo $(color_green "[linked]")
+    fi
+}
 
 setup_hooks() {
 	
+    cd $CURRENT_PATH
+
+    if git rev-parse --git-dir >& /dev/null;
+    then
+        
+        log "Installing hook links:"
+
+        #link_hook "$CURRENT_PATH/.git/hooks/commit-msg"
+        #link_hook "$CURRENT_PATH/.git/hooks/post-update"
+        link_hook "$CURRENT_PATH/.git/hooks/pre-commit"
+        #link_hook "$CURRENT_PATH/.git/hooks/prepare-commit-msg"
+        #link_hook "$CURRENT_PATH/.git/hooks/pre-push"
+        #link_hook "$CURRENT_PATH/.git/hooks/pre-rebase"
+        #link_hook "$CURRENT_PATH/.git/hooks/update"
+
+    else
+
+        fail "Current path is not a git working directory"
+    fi
 }
 
 update()
 {
-	mkdir -p $GITHOOKS_PATH
-	
-	cd $GITHOOKS_PATH
+    if [ -d "$GITHOOKS_PATH" ];
+    then
+        
+        echo -n "Changing to $GITHOOKS_PATH and update..."
 
-	git clone "$REPO"
+        cd $GITHOOKS_PATH && git pull origin master >& /dev/null
+
+        echo $(color_green "[OK]")
+
+    else
+
+    	mkdir -p $GITHOOKS_PATH
+        
+        log "Clone hooks repository in $GITHOOKS_PATH..."
+
+    	cd $GITHOOKS_PATH
+        git init . >& /dev/null
+        git remote add origin "$REPO"
+
+        update
+    fi
 }
 
 install()
 {
 	update 
+
+    setup_hooks
+
+    echo $(color_green "Done, happy coding!")
 }
 
 install "$@"
