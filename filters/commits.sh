7a8dac3e2be6d5bdb5c427bc1f0d80e6b06453c6
Cosmin Albulescu <cosmin.albulescu@digital-education.com>
2014-11-28 13:41:17 +0200

    Fix bug that adds matches twice

--- filters/commits.sh
+++ filters/commits.sh
@@ -21,7 +21,9 @@ then
 
             if [ "${#BASH_REMATCH[@]}" -eq "2" ]; then
                 
-                PREPEND=${BASH_REMATCH[1]}
+                MATCH_PREPEND_BRANCH_NAME=${BASH_REMATCH[1]}
+
+                PREPEND=$MATCH_PREPEND_BRANCH_NAME
 
                 if [ -n $PREPEND_BRANCH_MATCH_SUFFIX ]; then
                     PREPEND="$PREPEND$PREPEND_BRANCH_MATCH_SUFFIX"
@@ -31,9 +33,17 @@ then
                     PREPEND="$PREPEND_BRANCH_MATCH_PREFIX$PREPEND"
                 fi
 
-                COMMIT=$(cat $1)
+                INITIAL_COMMIT=$(cat $1)
+                COMMIT="$PREPEND$INITIAL_COMMIT"
 
-                echo -n "$PREPEND$COMMIT" > "$1"
+                PREPEND_BRANCH_MATCH_OCCURENCES=$(grep -o $MATCH_PREPEND_BRANCH_NAME <<< $COMMIT | wc -l)
+
+                if [ "$PREPEND_BRANCH_MATCH_OCCURENCES" != "1" ]; then
+                    deny "COMMITS" "prepend_branch_name is activated and match is present twice. Try to commit without: $MATCH_PREPEND_BRANCH_NAME"
+                    exit 1
+                fi
+
+                echo -n "$COMMIT" > "$1"
 
             else
                 notify-send -u critical -t 7000 "Git Hooks" "prepend_branch_match has no match to return"
